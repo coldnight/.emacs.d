bcdc2623aa27d02ddbb910e3f323db6437e5bc9e
wanghui <wh_linux@126.com>
2018-08-30 12:20:43 +0800

    Add mozilla irc

--- lisp/init-erc.el
+++ lisp/init-erc.el
@@ -13,14 +13,23 @@
                     "gnutls-cli --priority secure256"
                     "gnutls-cli --priority secure256 -p %p %h"))
 
- ; M-x start-irc
- (defun start-irc ()
-   "Connect to IRC."
-   (interactive)
-   (erc-tls :server "irc.freenode.net" :port 7000
-        :full-name secret-freenode-full-name
-        :nick secret-freenode-nick
-        :password secret-freenode-password))
+; M-x start-irc
+(defun start-irc ()
+  "Connect to IRC."
+  (interactive)
+  (erc-tls :server "irc.freenode.net" :port 7000
+           :full-name secret-freenode-full-name
+           :nick secret-freenode-nick
+           :password secret-freenode-password))
+
+
+(defun start-mozilla-irc ()
+  "Connect to Mozilla-irc."
+  (interactive)
+  (erc-tls :server "irc.mozilla.org" :port 6697
+           :full-name secret-freenode-full-name
+           :nick secret-freenode-nick
+           :password secret-freenode-password))
 
 (provide 'init-erc)
 ;; init-erc.el ends here
