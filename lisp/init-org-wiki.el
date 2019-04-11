99641ac114c9fd331edc2de0a97d07bcfd4211be
wanghui <wh_linux@126.com>
2019-04-11 20:33:03 +0800

    Add init-org-wiki

---
+++ lisp/init-org-wiki.el
@@ -0,0 +1,30 @@
+;;; init-org-wiki.el
+;;;
+;;; Commentary:
+;;;
+;;;   配置 org-wiki 用于笔记记录
+;;;
+;;; Code:
+
+
+(require 'org-wiki)
+
+(setq org-wiki-location "~/codes/incubating/notes")
+(setq org-wiki-template
+      (string-trim
+"
+#+TITLE: %n
+#+DESCRIPTION:
+#+KEYWORDS:
+#+STARTUP:  content
+#+DATE: %d
+#+SETUPFILE: https://fniessen.github.io/org-html-themes/setup/theme-bigblow.setup
+
+- [[wiki:index][Index]]
+
+- Related:
+
+* %n
+"))
+(provide 'init-org-wiki)
+;; ends init-org-wiki.el
