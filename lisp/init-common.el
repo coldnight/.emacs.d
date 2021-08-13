36273c585e9f0b898f111caef4453747a050fa67
wanghui <wh_linux@126.com>
2021-08-13 10:49:10 +0800

    remove time from modeline

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -90,18 +90,16 @@
 (setq tab-width 4) ; or any other preferred value
 
 ;; 时间显示设置
-;; 启用时间显示设置,在minibuffer上面的那个杠上
-(display-time-mode 1)
-;; 时间使用24小时制
-(setq display-time-24hr-format t)
-;; 时间显示包括日期和具体时间
-(setq display-time-day-and-date t)
-;; 时间栏旁边启用邮件设置
-(setq display-time-use-mail-icon t)
-;; 时间的变化频率
-(setq display-time-interval 10)
-;; 显示时间的格式
-(setq display-time-format "%A %H:%M")
+;; <s>启用时间显示设置,在minibuffer上面的那个杠上</s>
+;; 禁用时间显示
+(display-time-mode -1)
+;; (setq
+;;  display-time-24hr-format t  ;; 时间使用24小时制
+;;  display-time-day-and-date t ;; 时间显示包括日期和具体时间
+;;  display-time-use-mail-icon t ;; 时间栏旁边启用邮件设置
+;;  display-time-interval 10 ;; 时间的变化频率
+;;  setq display-time-format "%A %H:%M" ;; 显示时间的格式
+;;  )
 
 ;;
 (setq-local default-directory "~/.emacs.d/data/autosave")
