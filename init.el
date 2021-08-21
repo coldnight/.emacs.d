314935af28baa7b2b81e807a1cb128d3b7204f43
wanghui <wh_linux@126.com>
2021-08-21 08:27:48 +0800

    dashboard

--- init.el
+++ init.el
@@ -105,6 +105,39 @@
                      (recents . 5)
                      (agenda . 5)
                      (registers . 5)))
+  (dashboard-set-navigator t)
+  ;; Format: "(icon title help action face prefix suffix)"
+  (dashboard-navigator-buttons
+        `(;; line1
+          ((,(all-the-icons-octicon "mark-github" :height 1.1 :v-adjust 0.0)
+            "Homepage"
+            "Browse homepage"
+            (lambda (&rest _) (browse-url "http://github.com/coldnight")))
+           (,(all-the-icons-octicon "mail" :height 1.1 :v-adjust 0.0)
+            "Mail"
+            "Open mu4e"
+            (lambda (&rest _) (counsel-M-x "^mu4e$")))
+           (,(all-the-icons-octicon "issue-opened" :height 1.1 :v-adjust 0.0)
+            "Help"
+            "?/h"
+            (lambda (&rest _) (counsel-M-x "^help$")))
+           (,(all-the-icons-octicon "x" :height 1.1 :v-adjust 0.0)
+            "Quit"
+            "Quit Emacs"
+            (lambda (&rest _) (counsel-M-x "^kill-emacs$"))))
+          ((,(all-the-icons-octicon "sync" :height 1.1 :v-adjust 0.0)
+            "Update"
+            "Upate straight.el Packages."
+            (lambda (&rest _) (counsel-M-x "^straight-pull-all$")) nil "<" ">")
+           (,(all-the-icons-octicon "pin" :height 1.1 :v-adjust 0.0)
+            "Freeze"
+            "Freeze straight.el packages."
+            (lambda (&rest _) (counsel-M-x "^straight-freeze-versions$")) nil "<" ">")
+           (,(all-the-icons-octicon "clock" :height 1.1 :v-adjust 0.0)
+            "Thaw"
+            "Thaw straight.el packages to latest version."
+            (lambda (&rest _) (counsel-M-x "^straight-thaw-versions$")) nil "<" ">"))))
+  (dashboard-projects-switch-function 'counsel-projectile-switch-project-by-name)
   :init
   (dashboard-setup-startup-hook)
   (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))))
