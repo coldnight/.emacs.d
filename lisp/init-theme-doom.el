04375cd3c95a4e05cab79981df4930f6c3701cef
wanghui <wh_linux@126.com>
2021-12-17 11:29:26 +0800

    move setq to custom

--- lisp/init-theme-doom.el
+++ lisp/init-theme-doom.el
@@ -11,10 +11,11 @@
   :ensure t
   :straight (doom-themes :host github :repo "hlissner/emacs-doom-themes"
                          :files ("*.el" "themes"))
-  :config
+  :custom
   ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (doom-themes-enable-bold t)    ; if nil, bold is universally disabled
+  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  :config
   ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
   ;; may have their own settings.
   ;; (load-theme 'doom-nord t)
