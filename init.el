aab5ef804233553f3581af12743c10a256d9bbcb
wanghui <wh_linux@126.com>
2021-12-28 08:42:14 +0800

    remove: page-break-lines

--- init.el
+++ init.el
@@ -61,7 +61,7 @@
   :if (memq window-system '(x)))
 
 ;;; Appearance
-(use-package init-common-mode :defer 1)
+(use-package init-common-mode :defer 1 :after init-common)
 ;; (use-package init-theme-doom)
 ;; (use-package init-theme-nano)
 (use-package init-theme-bespoke)
@@ -138,12 +138,6 @@
   ;; Enables ligature checks globally in all buffers. You can also do it
   ;; per mode with `ligature-mode'.
   (global-ligature-mode t))
-
-(use-package page-break-lines
-  :straight t
-  :after init-common-mode
-  :config
-  (global-page-break-lines-mode))
 
 ;;; Shell
 (use-package exec-path-from-shell
