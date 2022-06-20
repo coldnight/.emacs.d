cf784065e2d1ded82c0f6e38bbd9b3226a58036d
wanghui <wh_linux@126.com>
2022-06-20 10:32:39 +0800

    add vterm-toggle

--- init.el
+++ init.el
@@ -135,6 +135,29 @@
   (defun vmacs-term-mode-p(&optional args)
     (derived-mode-p 'eshell-mode 'term-mode 'shell-mode 'vterm-mode)))
 
+(use-package vterm-toggle
+	:straight t
+	:after vterm
+	:custom
+	(vterm-toggle-fullscreen-p nil)
+	(vterm-toggle-cd-auto-create-buffer nil)
+	:bind
+	("C-c v t" . vterm-toggle-cd)
+	:config
+	(add-to-list 'display-buffer-alist
+							 '((lambda (buffer-or-name _)
+                   (let ((buffer (get-buffer buffer-or-name)))
+                     (with-current-buffer buffer
+                       (or (equal major-mode 'vterm-mode)
+                           (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
+                 (display-buffer-reuse-window display-buffer-at-bottom)
+                 ;;(display-buffer-reuse-window display-buffer-in-direction)
+                 ;;display-buffer-in-direction/direction/dedicated is added in emacs27
+                 ;;(direction . bottom)
+                 ;;(dedicated . t) ;dedicated is supported in emacs27
+                 (reusable-frames . visible)
+                 (window-height . 0.3))))
+
 (use-package multi-vterm
   :straight t
   :bind
