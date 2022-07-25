724ddae8e34eb2ccb67ce16e887a8a293fd1e200
wanghui <wh_linux@126.com>
2022-07-25 19:11:14 +0800

    feat: migrate theme from modus to color-theme-sanityinc

--- lisp/init-theme-modus.el
+++ lisp/init-theme-modus.el
@@ -8,22 +8,28 @@
   (add-to-list 'load-path (expand-file-name "straight/repos/use-package" user-emacs-directory))
   (require 'use-package))
 
-(use-package modus-themes
+;;; (use-package modus-themes
+;;;   :straight t
+;;;   :init
+;;;   ;; Add all your customizations prior to loading the themes
+;;;   (setq modus-themes-italic-constructs t
+;;;         modus-themes-bold-constructs nil
+;;;         modus-themes-region '(bg-only no-extend))
+;;;
+;;;   ;; Load the theme files before enabling a theme
+;;;   (modus-themes-load-themes)
+;;;   :config
+;;;   ;; Load the theme of your choice:
+;;;   ;; (modus-themes-load-operandi)
+;;;   ;; OR
+;;;   (modus-themes-load-vivendi)
+;;;   :bind ("<f5>" . modus-themes-toggle))
+;;;
+
+(use-package color-theme-sanityinc-tomorrow
   :straight t
   :init
-  ;; Add all your customizations prior to loading the themes
-  (setq modus-themes-italic-constructs t
-        modus-themes-bold-constructs nil
-        modus-themes-region '(bg-only no-extend))
-
-  ;; Load the theme files before enabling a theme
-  (modus-themes-load-themes)
-  :config
-  ;; Load the theme of your choice:
-  ;; (modus-themes-load-operandi)
-  ;; OR
-  (modus-themes-load-vivendi)
-  :bind ("<f5>" . modus-themes-toggle))
+  (color-theme-sanityinc-tomorrow-bright))
 
 ;; Dim inactive windows
 (use-package dimmer
